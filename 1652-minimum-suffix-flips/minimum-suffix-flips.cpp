341d99e57b08f8d5243e3b15a84946444ab4f2ce
DeepakReddy1999 <52119097+DeepakReddy1999@users.noreply.github.com>
2024-06-25 16:33:52 -0700

    Time: 10 ms (87.71%) | Memory: 10.6 MB (71.25%) - LeetSync

---
+++ 1652-minimum-suffix-flips/minimum-suffix-flips.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minFlips(string target) {
+        int n=target.size();
+        int ans=0;
+        if(target[0]=='1'){
+            ans++;
+        }
+        for(int i=0;i<n-1;i++){
+            if(target[i]!=target[i+1]){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
