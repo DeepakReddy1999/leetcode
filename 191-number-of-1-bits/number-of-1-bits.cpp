5ab2843e1a21d999582a4d9e197fc952fa977946
DeepakReddy1999 <52119097+DeepakReddy1999@users.noreply.github.com>
2024-10-15 17:25:04 -0700

    Time: 0 ms (100.00%) | Memory: 7.9 MB (77.04%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        int ans=0;
-        while(n !=0){
-            ans++;
-            n&=(n-1);
+    int hammingWeight(int n) {
+        int count=0;
+        for(int i=0;i<32;i++){
+            count+=(n>>i)&1;
         }
-        return ans;
+        return count;
     }
 };
